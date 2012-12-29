2dd6611fe419b1e133fdb227f8931ab0c34b373d
Gideon Dsouza <gideon@Gideons-Mac-mini.local>
2012-12-29 20:24:33 +0530

    Added github authentication with LWP::UserAgent. Removed login.tt since it isnt necessary anymore

--- dancr.pl
+++ dancr.pl
@@ -5,6 +5,11 @@ use DBI;
 use File::Spec;
 use File::Slurp;
 use Template;
+#we'll use this to send requests to github for authentication
+use LWP::UserAgent;
+#we'll use this to parse json responses from github
+use JSON::Parse 'json_to_perl';
+
  
 set 'database'     => File::Spec->catfile(File::Spec->tmpdir(), 'dancr.db');
 set 'session'      => 'Simple';
@@ -19,7 +24,26 @@ set 'password'     => 'password';
 set 'layout'       => 'main';
  
 my $flash;
- 
+
+my $client_id = "YOUR CLIENT ID HERE";
+my $client_secret = "YOUR SECRET ID HERE";
+
+#this is a utility method that will convert a query string into a hash
+sub parse_query_str {
+	my $str = shift;
+	my %in = ();
+	if (length ($str) > 0){
+	      my $buffer = $str;
+	      my @pairs = split(/&/, $buffer);
+	      foreach my $pair (@pairs){
+	           my ($name, $value) = split(/=/, $pair);
+	           $value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C", hex($1))/eg;
+	           $in{$name} = $value; 
+	      }
+	 }
+	return %in;
+}
+
 sub set_flash {
        my $message = shift;
  
@@ -54,7 +78,29 @@ hook before_template => sub {
        $tokens->{'login_url'} = uri_for('/login');
        $tokens->{'logout_url'} = uri_for('/logout');
 };
- 
+get '/auth/github/callback' => sub {
+	my $code                   = params->{'code'};
+	my $browser                = LWP::UserAgent->new;
+	my $resp                   = $browser->post('https://github.com/login/oauth/access_token',
+		[
+ 			client_id                  => $client_id,
+ 			client_secret              => $client_secret, 
+ 			code                       => $code,
+ 			state                      => 'x12'
+		]);
+	die "error while fetching: ", $resp->status_line
+	    unless $resp->is_success;
+	my %querystr = parse_query_str($resp->decoded_content);
+	my $acc = $querystr{access_token};
+	
+	my $jresp  = $browser->get("https://api.github.com/user?access_token=$acc");
+	my $json = json_to_perl($jresp->decoded_content);
+	session 'username' => $json->{login};
+	session 'avatar' => $json->{avatar_url};
+	session 'logged_in' => true;
+	redirect "/";
+};
+
 get '/' => sub {
        my $db = connect_db();
        my $sql = 'select id, title, text from entries order by id desc';
@@ -80,31 +126,11 @@ post '/add' => sub {
        set_flash('New entry posted!');
        redirect '/';
 };
- 
-any ['get', 'post'] => '/login' => sub {
-       my $err;
- 
-       if ( request->method() eq "POST" ) {
-               # process form input
-               if ( params->{'username'} ne setting('username') ) {
-                       $err = "Invalid username";
-               }
-               elsif ( params->{'password'} ne setting('password') ) {
-                       $err = "Invalid password";
-               }
-               else {
-                       session 'logged_in' => true;
-                       set_flash('You are logged in.');
-                       return redirect '/';
-               }
-       }
- 
-       # display login form
-       template 'login.tt', {
-               'err' => $err,
-       };
- 
+
+get '/login' => sub {
+	redirect "https://github.com/login/oauth/authorize?&client_id=$client_id&state=x12";
 };
+
  
 get '/logout' => sub {
        session->destroy;
